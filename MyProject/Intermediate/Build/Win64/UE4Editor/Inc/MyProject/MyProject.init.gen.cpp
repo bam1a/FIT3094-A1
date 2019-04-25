aa9954279c765cf6640465021f37818eb9abcce4
Bon Chau <tccha17@student.monash.edu>
2019-04-26 03:41:33 +1000

    setup wall class

--- MyProject/Intermediate/Build/Win64/UE4Editor/Inc/MyProject/MyProject.init.gen.cpp
+++ MyProject/Intermediate/Build/Win64/UE4Editor/Inc/MyProject/MyProject.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeMyProject_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/MyProject",
 				PKG_CompiledIn | 0x00000000,
-				0x9A77A137,
-				0x539DE5B9,
+				0x2009E26E,
+				0x662A8CAD,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
